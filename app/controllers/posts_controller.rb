111634a4201be3a6012390119a97ccb06eb0d1ef
Rafael Montas <rafaelmontas1@gmail.com>
2022-06-04 01:58:36 -0400

    Add new and create actions with Turbo Frames

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,31 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all()
+    @posts = Post.all
   end
+
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      render turbo_stream: [
+        turbo_stream.prepend("posts", @post),
+        turbo_stream.replace("form_post", partial: "form", locals: { post: Post.new })
+      ]
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
 end
