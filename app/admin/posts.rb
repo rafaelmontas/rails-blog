b81ec4e92a9f77eac8bdc5b00773e3550ffbd0a5
Rafael Montas <rafaelmontas1@gmail.com>
2022-06-07 18:12:38 -0400

    Edit tags via active admin

--- app/admin/posts.rb
+++ app/admin/posts.rb
@@ -1,5 +1,22 @@
 ActiveAdmin.register Post do
-  permit_params :title, :body, :tags, :category_name
+  permit_params :title, :body, :tags_raw, :category_name
+
+  form do |f|
+    f.inputs "Post" do
+      f.input :category 
+      f.input :title
+      f.input :body
+      f.input :tags_raw, :as => :string
+    end
+    f.actions
+  end
+
+  controller do
+    def update
+      puts params[:post][:tags_raw]
+      super
+    end
+  end
   
   # permit_params do
   #   permitted = [:title, :body, :tags, :category]
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,16 @@
 class Post < ApplicationRecord
   belongs_to :category, foreign_key: 'category_name', optional: true
 
+  def tags_raw
+    self.tags.join(",") unless self.tags.nil?
+  end
+
+  def tags_raw=(values)
+    self.tags = []
+    self.tags=values.split(",")
+  end
+
+
   validates :title, presence: true
   validates :body, presence: true
 end
