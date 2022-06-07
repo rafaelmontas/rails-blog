e6d9ec32ceade44c1e50a074dbac40d645730842
Rafael Montas <rafaelmontas1@gmail.com>
2022-06-07 17:20:53 -0400

    Finish styles for edit categories, tags, and edits

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   def show
     @category = Post.where(category: params[:name]).order("created_at DESC")
+    @category_name = params[:name]
   end
 end
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,6 @@
 class TagsController < ApplicationController
   def show
     @tag = Post.where("'#{params[:name]}' = ANY (tags)").order("created_at DESC")
+    @tag_name = params[:name]
   end
 end
